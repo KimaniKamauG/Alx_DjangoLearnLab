6b74c20d0333b6209b8949ff6c6932e3cb54401f
Kimani Kamau <kimkamaug@gmail.com>
2024-11-01 10:25:33 +0300

    Included the @admin.register(Book) command

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Book
 
-# Register your models here.
+# Register your models here
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
     # Displays the title, author, and publication_year fields in the list view.
