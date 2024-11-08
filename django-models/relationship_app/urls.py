65507eff07636c3fef6368f2d1deea51908350c2
Kimani Kamau <kimkamaug@gmail.com>
2024-11-08 23:40:22 +0300

    Included the views.register.

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('books/', list_books, name='list_books'), # Function based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), # Class based view
-    path('register', register_user, name='register'),
+    path('register', register_user, name='views.register'),
     path('login', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout', LogoutView.as_view( template_name='relationship_app/logout.html'), name='logout')
+    path('logout', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout')
 ]
