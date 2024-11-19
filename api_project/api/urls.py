517114088c3b5bd32f42867ad98388f3b99db373
Kimani Kamau <kimkamaug@gmail.com>
2024-11-20 00:37:09 +0300

    Implemented authentication and permissions in DRF

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include 
 from rest_framework.routers import DefaultRouter 
 from .views import BookList, BookViewSet 
+from rest_framework.authtoken.views import obtain_auth_token 
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -9,5 +10,7 @@ urlpatterns = [
     # Route for the BookList view (ListAPIView)
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view 
     # Router for the BookViewSet (all CRUD Operations)
-    path('', include(router.urls)),  # This includes all routes registered with the router 
+    path('', include(router.urls)),  # This includes all routes registered with the router
+    # Url path for the Token Retrieval Endpoint 
+    path('api-token-auth/', obtain_auth_token), 
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics 
 from .serializers import BookSerializer 
 from .models import Book 
-from rest_framework import viewsets 
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response 
 
 # Create your views here.
 
@@ -11,5 +13,9 @@ class BookList(generics.ListAPIView):
     serializer_class = BookSerializer 
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer 
+    permission_classes = [IsAuthenticated]  # Only authenticated users can access this view.
+
+    def get(self, request):
+        queryset = Book.objects.all()
+        serializer = BookSerializer(queryset, many=True)
+        return Response(serializer.data) 
